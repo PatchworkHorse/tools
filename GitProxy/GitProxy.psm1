6bcb04c49ab68679030d509b941b565753f100cd
PatchworkHorse <129221931+PatchworkHorse@users.noreply.github.com>
2024-10-24 11:06:12 -0400

    Update GitProxy.psm1

--- GitProxy/GitProxy.psm1
+++ GitProxy/GitProxy.psm1
@@ -1,3 +1,4 @@
+# Functions wearing this attribute will be discovered and registered as git commands.
 class GitAliasAttribute : System.Attribute {
     [string]$Alias
     GitAliasAttribute([string]$alias) {
@@ -5,15 +6,18 @@ class GitAliasAttribute : System.Attribute {
     }
 }
 
+# Discovers and registers all functions wearing the GitAlias attribute as git commands
+# Todo: We duplicate searching for functions in the module. Instead we should update Get-AvailableFunctions to also include the GitAlias attribute value
 function Initialize-GitAliases {
-    # Register the git aliases for each function wearing the GitAlias attribute
     Get-AvailableFunctions | ForEach-Object {
         $alias = $_.ScriptBlock.Attributes | Where-Object { $_.GetType().Name -eq "GitAliasAttribute" } | Select-Object -ExpandProperty Alias
+        Write-Host "Registering git alias: $alias"
         & git config --global alias.$alias "!powershell -NoProfile -Command Invoke-GitCommand -Command $alias"
     }
 
 }
 
+# Returns all functions in the module that wear the GitAlias attribute
 function Get-AvailableFunctions {
     Get-Command -Module $MyInvocation.MyCommand.Module.Name -CommandType Function | Where-Object {
         $_.ScriptBlock.Attributes | Where-Object { $_.GetType().Name -eq "GitAliasAttribute" }
@@ -43,13 +47,25 @@ function Invoke-GitCommand {
 
 ####
 # Below are the functions that will be available as git commands
+# Be sure to add the GitAlias attribute to each function so it will be resolved as a git command
 ####
 
 function HelloWorld {
     [GitAlias("hello")]
     param()
 
-    Write-Host "Hello, World!"
+    Write-Host "Hello, World! GitProxy has been successfully installed."
 } 
 
-Export-ModuleMember -Function Initialize-GitAliases, Invoke-GitCommand
+function PruneMerged {
+    [GitAlias("prune-merged")]
+    param()
+
+    # Todo: We may need to force deletion (-D) if we're confident that the branches are merged remotely 
+    foreach ($b in (git branch --merged main | Where-Object { $_ -notin ("* main", "* master") } )) {
+         git branch -d $b.Trim()
+    }
+}
+
+# Keep this at the end of the file
+Export-ModuleMember -Function Initialize-GitAliases, Invoke-GitCommand, Get-AvailableFunctions
