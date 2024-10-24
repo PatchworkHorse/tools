84e6d37512a8e08b2fa8223ed68ce39d2e08d626
PatchworkHorse <129221931+PatchworkHorse@users.noreply.github.com>
2024-10-24 11:06:23 -0400

    Update install.ps1

--- GitProxy/install.ps1
+++ GitProxy/install.ps1
@@ -1,3 +1,11 @@
-Copy-Item -Path $PSScriptRoot -Destination $env:PSModulePath.Split(';')[0] -Recurse
-Import-Module GitProxy
-Install-GitAliases
+# Copies the module to the first path in the PSModulePath environment variable and imports the module.
+Copy-Item -Path $PSScriptRoot -Destination $env:PSModulePath.Split(';')[0] -Recurse -Force
+
+# Remove any pre-existing module and import the new module. (May be able to remove this since we force import in a subsequent step)
+Uninstall-Module GitProxy -Force -ErrorAction SilentlyContinue
+
+# Import the module
+Import-Module -Name .\GitProxy.psm1 -Force
+
+# Register the git aliases
+Initialize-GitAliases
