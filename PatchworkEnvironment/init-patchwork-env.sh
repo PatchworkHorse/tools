9ff575a329207081f0e44e4466b378160ec1dedd
Patchwork Horse <paintcolt@gmail.com>
2025-03-10 20:48:16 -0400

    Add unzip install to init

--- PatchworkEnvironment/init-patchwork-env.sh
+++ PatchworkEnvironment/init-patchwork-env.sh
@@ -6,6 +6,13 @@
 # Install oh-my-posh
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 
+# Install unzip (if not already installed)
+if ! command -v unzip &> /dev/null
+then
+    echo "unzip could not be found, installing..."
+    brew install unzip
+fi
+
 # Download and unzip themes
 mkdir -p ~/.poshthemes
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
